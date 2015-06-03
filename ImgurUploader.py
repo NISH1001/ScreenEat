ac7f5da69a66f1462740b1f8a4e70f434d0716d2
bibek <bibekdahal.bd16@gmail.com>
2015-06-03 14:38:31 +0545

    Imgur Uploader added

---
+++ ImgurUploader.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import base64, requests
+
+"""
+An uploader class to upload image to imgur
+Upload : Method to upload a file to imgur
+"""
+class ImgurUploader():
+    def __init__(self):
+        client_id = "e8ac1a4090ab0b3"
+        self.headers = {"Authorization": "Client-ID "+client_id}
+
+
+    def Upload(self, filename):
+        try:
+            # upload route
+            url = "https://api.imgur.com/3/image"
+            # POST with file data encoded in base64
+            response = requests.post(
+                url,
+                headers=self.headers,
+                data = {
+                    'image': base64.b64encode(open(filename, 'rb').read()),
+                    'type': 'base64',
+                    'name': filename,
+                    'title': 'ScreenEat Upload'
+                }
+            )
+
+            response_json = response.json()
+
+            result = {}
+            result["success"] = response_json["success"]
+            if (result["success"]):
+                result["link"] = response_json["data"]["link"]
+            return result
+        except:
+            return {'success': False}
+        
+
+def main():
+    imguru = ImgurUploader()
+    result = imguru.Upload("test.png")
+    print(result)
+
+
+if __name__=="__main__":
+    main()
