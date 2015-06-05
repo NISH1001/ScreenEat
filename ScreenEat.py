cfc127c337f00a3b8905d44ab78a5e05500a5dee
NISH1001 <nish_play1001@yahoo.com>
2015-06-05 23:34:07 +0545

    setting button added

--- ScreenEat.py
+++ ScreenEat.py
@@ -47,15 +47,24 @@ class ScreenEat(Gtk.Window):
         #button_save = Gtk.Button(label="Save To File")
         button_save = Gtk.Button(image=Gtk.Image(stock=Gtk.STOCK_SAVE_AS))
         button_save.props.margin_left = 10
+        button_save.set_tooltip_text("save as")
         button_save.connect("clicked", self.ImageSave, pixel_buffer) #pixel buffer is passed
         grid.attach(button_save, 2, 0, 1, 1)
 
         #button_copy = Gtk.Button(label="Copy Image To Clipboard")
         button_copy = Gtk.Button(image=Gtk.Image(stock=Gtk.STOCK_COPY))
         button_copy.props.margin_left = 10
+        button_copy.set_tooltip_text("copy to clipboard")
         #button_copy.set_sensitive(False)
         button_copy.connect("clicked", self.ImageCopy, pixel_buffer)
         grid.attach(button_copy, 3, 0, 1, 1)
+
+        button_settings = Gtk.Button(image=Gtk.Image(stock=Gtk.STOCK_PREFERENCES))
+        button_settings.props.margin_left = 10
+        button_settings.set_tooltip_text("settings")
+        #button_settings.set_size_request(3,4)
+        button_settings.connect("clicked", self.Configuration)
+        grid.attach(button_settings, 4,0, 1, 1)
 
         
         # create all 3 upload sections:
@@ -101,6 +110,12 @@ class ScreenEat(Gtk.Window):
 
         # connect the main window to keypress
         self.connect("key-press-event", self.KeyPress)
+
+    def Configuration(self, widget):
+        win = ConfigWindow.ConfigWindow()
+        #win.connect("delete-event", Gtk.main_quit)
+        win.show_all()
+        #Gtk.main()
 
     def KeyPress(self, widget, event):
         # if Escape -> 65307 is the code
