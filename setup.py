46718399358255031953a21f705b8d7536bfe9cc
NISH1001 <nishanpantha@gmail.com>
2018-08-13 08:23:14 +0545

    Add url and long description in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,11 @@ setup(
     name='ScreenEat',
     version='1.0',
     description='Screen Capturing Made Delicious',
+    long_description='Seamlessly take screenshots by directly uploading to imgur and get a shareable link',
+    url='https://github.com/NISH1001/ScreenEat',
     author='Nishan Pantha, Bibek Dahal, Safar Ligal, Bibek Pandey',
-    author_email='nishanpantha@gmail.com, bibek.dahal@togglecorp.com, weathermist@gmail.com, bewakepandey@gmail.com',  # noqa
+    author_email='nishanpantha@gmail.com, bibek.dahal@togglecorp.com, weathermist@gmail.com, bewakepandey@gmail.com',
+    license='GNU GPL V3',
     packages=['screen_eat', 'screen_eat.windows', 'screen_eat.uploaders'],
     package_data={
         '': ['*.glade']
